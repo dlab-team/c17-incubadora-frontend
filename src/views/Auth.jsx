d5c4f5bfdaf6ecdb73e6e6730475100e27a395fd
unknown <d.azaharvichp@gmail.com>
2024-01-31 14:42:40 -0300

    :sparkles:feat: Add Views

---
+++ src/views/Auth.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Auth() {
+  return (
+    <div>Auth</div>
+  )
+};
+
+export default Auth;
---
+++ src/views/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function home() {
+  return (
+    <div>home</div>
+  )
+};
+
+export default home;
