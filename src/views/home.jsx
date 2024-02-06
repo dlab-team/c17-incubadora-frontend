1f50e6321d6789b93f68aa0d44b108786ac6cf7d
Dominque Piffardi <domiskp@gmail.com>
2024-02-06 19:48:28 -0300

    footer added

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import BannerPrincipal from "../components/BannerPrincipal/BannerPrincipal";
 import SinJrsNoHaySeniors from "../components/SinJrsNoHaySeniors/SinJrsNoHaySeniors";
 import { ComoFuncionamos } from "../components/ComoFuncionamos/ComoFuncionamos";
+import Footer from "../components/footer/Footer";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <BannerPrincipal />
       <ComoFuncionamos />
       <SinJrsNoHaySeniors />
+      <Footer />
     </>
   );
 }
