14a9dea06bdcccd2f0d0a456f9625a24b7783d9b
Gustavo Arancibia <garancibiacl@gmail.com>
2024-02-07 01:45:31 -0300

    se crea la vista register

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import { CiMail } from "react-icons/ci";
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { CiLinkedin } from "react-icons/ci";
-import { FaTwitter } from "react-icons/fa";
-import { IoLogoGithub } from "react-icons/io";
+import Form from 'react-bootstrap/Form';
 
 
 import './register.css'
@@ -17,35 +12,62 @@ const Register = () => {
   
   <section className='bg_body_register pt-5'>
   <Container className=" bg-white container-start">
-      <Row>
-        <Col><h1 className="pt-5" >Únete a Devsafío</h1></Col>
-      </Row>
 
-      <Row className="justify-content-md-center">
-        <Col className="col-9"><p className="parrafo-register pt-5" >Crea tu cuenta profesional en Devsafío para que seas parte de distintas ofertas laborales que tenemos junto a importantes empresas en latinoamérica.</p></Col>
-      </Row>
+
+
 
       <Row>
-        <Col className="pt-4">
-        
-        <Button variant="outline-secondary"><CiMail />  Ingresa con tu mail</Button>
+        <Col><h1 className="pt-5" >Regístrate</h1></Col>
+      </Row>
+
+      
+
+      <Row className="justify-content-md-center pt-5 pt-3">
+        <Col className="col-md-auto"  xs={12} ><p className="text-start " >Nombre:</p> 
         </Col>
+        <Col xs={12} md={3}> 
+         <Form.Control type="text" placeholder="" />
+          </Col>
+
+          <Col className="col-md-auto" xs={12}  ><p className="text-start" >Apellido:</p> 
+        </Col>
+        <Col xs={12} md={3}> 
+         <Form.Control type="text" placeholder="" />
+          </Col>
+
       </Row>
 
-      <Row>
-        <Col className="pt-2">
-        
-        <Button className="mx-2" variant="outline-secondary"><AiFillGoogleCircle />  Ingresa con Google</Button>
-        <Button variant="primary"><CiLinkedin />  Ingresa con LinkedIn</Button>
+      <Row className="justify-content-md-center pt-3">
+      <Col className="col-md-auto" xs={12} ><p className="text-start" >Ingresa tu correo:</p> 
         </Col>
+        <Col xs={12} md={6}> 
+         <Form.Control type="text" placeholder="" />
+          </Col>
       </Row>
 
-      <Row>
-        <Col className="pt-2">
-        <Button className="mx-2" variant="primary"><FaTwitter />  Ingresa con Twitter</Button>
-        <Button variant="dark"><IoLogoGithub />  Ingresa con GitHub</Button>
+      <Row className="justify-content-md-center pt-3">
+      <Col className="col-md-auto" xs={12} ><p className="text-start" >Crea tu contraseña:</p> 
         </Col>
+        <Col xs={12} md={6}> 
+         <Form.Control type="text" placeholder="" />
+          </Col>
+
+     
       </Row>
+
+      <Row className="justify-content-md-center pt-3">
+      <Col className="col-md-auto" xs={12} ><p className="text-start" >Reingresa tu contraseña:</p> 
+        </Col>
+        <Col xs={12} md={6}> 
+         <Form.Control type="text" placeholder="" />
+          </Col>
+
+     
+      </Row>
+
+   
+
+   
     </Container>
 
   </section>
