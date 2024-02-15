38aec47a91caf0d2cb4e8aabb145cb5af8a1aae4
Dominque Piffardi <domiskp@gmail.com>
2024-02-15 18:16:48 -0300

    conflictos resueltos

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,7 +4,6 @@ import Home from "../views/home";
 import Profile from "../views/Profile";
 import Error from "../views/Error";
 import Register from "../components/register/Register";
-import Profiles from "../components/profiles/Profiles";
 import ContactUs from "../components/contactUs/ContactUs";
 import Login from "../components/login/Login";
 import Formulario from "../views/Formulario";
@@ -19,8 +18,6 @@ const Router = () => {
       <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registrate" element={<Register />} />
